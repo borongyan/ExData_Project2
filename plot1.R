7779ae38b580c39a55c122dd6e8bf9d2b41edc9b
d30xy <mrogman@gmail.com>
2015-07-26 11:56:27 -0700

    plot1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+source('getEmissionData.R')
+
+emissionsPerYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(
+  height=emissionsPerYear$Emissions/10^6,
+  names.arg=emissionsPerYear$year,
+  xlab="years",
+  ylab=expression('total PM'[2.5]*' emission (Million Tons)'),
+  main=expression('Total PM'[2.5]*' emissions from US sources between 1999 and 2008')
+  )
+dev.off()
