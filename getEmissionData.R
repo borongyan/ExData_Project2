9d2d595a59782e123345a7992f0d2ef9d798deed
d30xy <mrogman@gmail.com>
2015-07-26 11:25:16 -0700

    retrieve data routine and gitignore

---
+++ getEmissionData.R
@@ -0,0 +1,26 @@
+## download zip if rds files does not exist
+retrieveDataFiles <- function() {
+  message('required files do not exist: downloading and extracting...')
+  zipFile <- "data.zip"
+  if(!file.exists(zipFile)) {
+    fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip'
+    if(Sys.info()['sysname'] == 'Darwin')
+      download.file(url = fileUrl, destfile = zipFile, method = 'curl')
+    else
+      download.file(url = url, destfile = archiveFile)
+  }
+  unzip(zipFile)
+  file.remove(zipFile)
+}
+
+## retrieve emission data if not found in environment
+if(!exists('NEI') | !exists('SCC')) {
+  message('required data frames do not exist')
+  ## check for RDS files
+  if(!file.exists('summarySCC_PM25.rds') && !file.exists('Source_Classification_Code.rds')) retrieveDataFiles()
+  ## read data from files
+  message('reading data...')
+  NEI <- readRDS("summarySCC_PM25.rds")
+  SCC <- readRDS("Source_Classification_Code.rds")
+  message('setup complete')
+}
