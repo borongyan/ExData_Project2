64271e29c71f5764ae94b3c88b191aecc1745ec7
d30xy <mrogman@gmail.com>
2015-07-26 12:11:06 -0700

    update plot1

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ barplot(
   height=emissionsPerYear$Emissions/10^6,
   names.arg=emissionsPerYear$year,
   xlab="years",
-  ylab=expression('total PM'[2.5]*' emission (Million Tons)'),
+  ylab=expression('total PM'[2.5]*' emission (megatons)'),
   main=expression('Total PM'[2.5]*' emissions from US sources between 1999 and 2008')
   )
 dev.off()
